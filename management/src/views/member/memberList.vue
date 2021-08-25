cb214efd7def8442b45c896a35985ec2d5ec236f
lanmeidada <lanmeidada@qq.com>
2021-08-25 17:22:07 +0800

    Add: 成员列表文件

---
+++ management/src/views/member/memberList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'MemberList',
+  data() {
+    return
+  },
+  create() {
+    this.getMemberList();
+  },
+  methods: {
+    getMemberList() {
+      this.$ajax({
+        url: '/member/list',
+        method: 'GET'
+      }).then(res => {
+
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
